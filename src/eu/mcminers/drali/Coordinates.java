1d2c7fc848c9250ebfd49cac827730ea1279b3b9
draliii <holl.dita@gmail.com>
2014-04-02 10:54:36 +0200

    Comments

---
+++ src/eu/mcminers/drali/Coordinates.java
@@ -0,0 +1,18 @@
+package eu.mcminers.drali;
+
+/**
+ * An object that holds the x and z coordinates
+ * @author drali_
+ */
+
+public class Coordinates {
+  public int x;
+  public int z;
+  
+  public Coordinates(int x, int z){
+    this.x = x;
+    this.z = z;
+  }
+}
+
+
